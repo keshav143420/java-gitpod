e993c6a165e4b30215437f252791e2ba0b90251d
keshav143420 <keshav143420@gmail.com>
2021-03-10 09:51:46 +0530

    updated gitpod config (#5)

--- app/src/test/java/com/kk/AppTest.java
+++ app/src/test/java/com/kk/AppTest.java
@@ -11,4 +11,9 @@ class AppTest {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
+
+    @Test
+    void sampleGreenTest(){
+        assertEquals(1, 2);
+    }
 }
